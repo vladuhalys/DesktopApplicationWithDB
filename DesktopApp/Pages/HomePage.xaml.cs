0307f6d443fc55c1079a9de3018a5860884cd8e3
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2025-03-31 20:33:46 +0300

    Add HomePage user control and implement logout functionality

---
+++ DesktopApp/Pages/HomePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopApp.Pages;
+
+public partial class HomePage : UserControl
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnLogoutClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
