652d41c8bb340af5ae0dff1a8018b491c6421707
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2025-04-07 19:15:58 +0300

    Add CloudeDatabase class and update project dependencies for Supabase

---
+++ SupabaseService/DataSource/CloudeDatabse.cs
@@ -0,0 +1,19 @@
+﻿namespace DesktopApplication.DataSource;
+
+public class CloudeDatabase
+{
+    public Supabase.Client SupabeseClient { get; private set; }
+    
+    public SupabaseClient()
+    {
+        var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+        var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+
+        var options = new Supabase.SupabaseOptions
+        {
+            AutoConnectRealtime = true
+        };
+
+        SupabeseClient = new Supabase.Client(url, key, options);
+    }
+}
