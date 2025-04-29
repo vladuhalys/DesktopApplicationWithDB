e0194d0257c95186129a2c12c473cd34970702a7
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2025-04-29 20:21:40 +0300

    Refactor GutenbergApiTests to improve structure and add comprehensive test cases for ApiService

--- GutenbergApiTest/GutenbergApiTests.cs
+++ GutenbergApiTest/GutenbergApiTests.cs
@@ -1,65 +1,70 @@
+using System.Net;
 using GutenbergApi.Models;
 using GutenbergApi.Services;
 using Moq;
 using Moq.Protected;
+using Newtonsoft.Json;
 
-namespace GutenbergApiTest;
-
-public class GutenbergApiTestsClass
-{
-    Mock<HttpClient> _httpClientMock;
-    Mock<HttpResponseMessage> _httpResponseMessageMock;
-    
-    [SetUp]
-    public void Setup()
+namespace GutenbergApiTest
     {
-        _httpClientMock = new Mock<HttpClient>();
-    }
-
-    [Test]
-    public void TestApiService()
-    {
-        var apiService = new ApiService(_httpClientMock.Object);
-        Assert.IsNotNull(apiService);
-    }
-    
-    [Test]
-    public void TestGetAllBooksWhenSuccess()
-    {
-        var apiService = new ApiService(_httpClientMock.Object);
-        var response = apiService.GetAllBooks();
-        
-        Assert.IsNotNull(response);
-        Assert.IsInstanceOf<Task<ListOfBooks>>(response);
-        Assert.IsInstanceOf<ListOfBooks>(response.Result);
-    }
-    
-    [Test]
-    public async Task TestGetAllBooksWhenApiError()
-    {
-        // Arrange
-        var mockHandler = new Mock<HttpMessageHandler>();
-        mockHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync", 
-                ItExpr.IsAny<HttpRequestMessage>(), 
-                ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = System.Net.HttpStatusCode.InternalServerError
-            });
-        
-        var httpClient = new HttpClient(mockHandler.Object)
+        [TestFixture]
+        public class ApiServiceTests
         {
-            BaseAddress = new Uri(UrlService.BaseUrl)
-        };
-        
-        var apiService = new ApiService(httpClient);
-        
-        // Act & Assert
-        var exception = Assert.ThrowsAsync<Exception>(async () => 
-            await apiService.GetAllBooks());
-        
-        Assert.That(exception.Message, Contains.Substring("Api error: Status code 500"));
+            [Test]
+            public async Task GetAllBooks_ReturnsListOfBooks_WhenResponseIsSuccessful()
+            {
+                // Arrange
+                var expectedBooks = new ListOfBooks(1, null, null, new List<Book>
+                {
+                    new Book(1, "Test Book")
+                });
+                var json = JsonConvert.SerializeObject(expectedBooks);
+    
+                var handlerMock = new Mock<HttpMessageHandler>();
+                handlerMock.Protected()
+                    .Setup<Task<HttpResponseMessage>>(
+                        "SendAsync",
+                        ItExpr.IsAny<HttpRequestMessage>(),
+                        ItExpr.IsAny<CancellationToken>())
+                    .ReturnsAsync(new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Content = new StringContent(json)
+                    });
+    
+                var httpClient = new HttpClient(handlerMock.Object);
+                var apiService = new ApiService(httpClient);
+    
+                // Act
+                var result = await apiService.GetAllBooks();
+    
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.That(result.Count, Is.EqualTo(1));
+                Assert.That(result.Results[0].Title, Is.EqualTo("Test Book"));
+            }
+    
+            [Test]
+            public void GetAllBooks_ThrowsException_WhenStatusCodeIsNotSuccess()
+            {
+                // Arrange
+                var handlerMock = new Mock<HttpMessageHandler>();
+                handlerMock.Protected()
+                    .Setup<Task<HttpResponseMessage>>(
+                        "SendAsync",
+                        ItExpr.IsAny<HttpRequestMessage>(),
+                        ItExpr.IsAny<CancellationToken>())
+                    .ReturnsAsync(new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.BadRequest
+                    });
+    
+                var httpClient = new HttpClient(handlerMock.Object);
+                var apiService = new ApiService(httpClient);
+    
+                // Act & Assert
+                var ex = Assert.ThrowsAsync<Exception>(async () => await apiService.GetAllBooks());
+                Assert.That(ex.Message, Does.Contain("Status code"));
+            }
+        }
     }
-}
